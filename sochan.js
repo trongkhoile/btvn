175dedf9a93b1c98e283a537da5b29f5c388efc1
trongkhoile <80833913+trongkhoile@users.noreply.github.com>
2021-07-24 22:32:01 +0700

    Add files via upload

---
+++ sochan.js
@@ -0,0 +1,68 @@
+let a=prompt("nhập số từ 4 đến 20")
+if (a <4  || a > 20){
+ console.log("số bạn điền không hợp lệ")
+} 
+else if(a==4 || a==5){
+    for (var i = 4; i <= 5; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+
+else if( a==6 || a==7){
+    for (var i = 4; i <= 7; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==8 || a==9){
+    for (var i = 4; i <= 9; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==10 || a==11){
+    for (var i = 4; i <= 11; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==12 || a==13){
+    for (var i = 4; i <= 13; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==14 || a==15){
+    for (var i = 4; i <= 15; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==16 || a==17){
+    for (var i = 4; i <= 17; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if( a==18 || a==19){
+    for (var i = 4; i <= 19; i++){
+        if (i % 2 == 0){
+            console.log(i);
+        }
+    }
+}
+else if (a==20) {
+for (var i = 4; i <= 20; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
+}
